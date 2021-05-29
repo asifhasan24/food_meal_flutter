0fe285d1e950042478e585a3f2871f440868767e
asifhasan24 <asifmehu24@gmail.com>
2021-05-30 00:01:12 +0600

    finished

--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/category_meals_screen.dart';
+
+import '../screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
@@ -9,11 +10,13 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    // Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-    //   return CategoryMealsScreen(id, title);
-    // }));
-    Navigator.of(ctx).pushNamed(CategoryMealsScreen.routeName,
-        arguments: {'id': id, 'title': title});
+    Navigator.of(ctx).pushNamed(
+      CategoryMealsScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
   }
 
   @override
